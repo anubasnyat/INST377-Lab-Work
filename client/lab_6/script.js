9b51737b334ddb37598df995eb7d89835ddff042
anubasnyat <112788215+anubasnyat@users.noreply.github.com>
2022-10-21 23:19:35 -0400

    commit final

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -19,6 +19,16 @@ function getRandomIntInclusive(min,max) {
 
 function injectHTML(list) {
   console.log('fired injectHTML');
+  const target = document.querySelector("#restaurant_list");
+  target.innerHTML='';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
+  list.forEach((item) => {
+      const el = document.createElement('li');
+      el.innerText = item.name;
+      listEl.appendChild(el);
+  });
   /*
   ## JS and HTML Injection
     There are a bunch of methods to inject text or HTML into a document using JS
@@ -41,11 +51,9 @@ function processRestaurants(list) {
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
     return list[index];
-  })
+  });
   return newArray;
-}
-
-/*
+  /*
     ## Process Data Separately From Injecting It
       This function should accept your 1,000 records
       then select 15 random records
@@ -63,7 +71,7 @@ function processRestaurants(list) {
     - Return only their name, category, and location
     - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
   */
-
+}
 async function mainEvent() {
   /*
     ## Main Event
